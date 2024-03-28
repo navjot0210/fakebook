92e1b4bffac4711874df39b1c8e7db758b5baf9b
Navjot Kaur <saininavjot357019@gmail.com>
2024-03-28 10:03:03 -0500

    Striving for Perfection

--- assets/script/app.js
+++ assets/script/app.js
@@ -15,8 +15,12 @@ const profileImg = select('.profile');
 const postButton = select('.post');
 const postSection = select('.display-post')
 const textArea = select('textarea');
+const userInfo = document.getElementById('modal');
+const overlay = select('.overlay');
 
 const now = new Date();
+let isVisible = false;
+let imageSrc;
 
 listen('change', postImage, () => {
   let fileName = postImage.files[0].name;
@@ -29,8 +33,7 @@ function clearInputs() {
   fileDetail.textContent = '';
 }
 
-function makePost() {
-  let imageSrc;
+function makeImage() {
 
   (postImage.files[0]) ? imageSrc = 
   `
@@ -38,23 +41,31 @@ function makePost() {
     <img src="${URL.createObjectURL(postImage.files[0])}" class="posted-image">
   </figure>
   `: imageSrc = '';
+}
 
-  const newPost = document.createElement('div');
-  postSection.appendChild(newPost);
-  newPost.className = `the-post data`;
-  newPost.innerHTML += `
-  <header class="post-header flex space-between">
-    <div class="flex gap-15">
-      <figure>
-        <img src="./assets/media/profile.jpeg" class="profile">
-      </figure>
-      <h3>Navjot Kaur</h3>
-    </div>
-    <p>${now.toDateString().split(' ').slice(1).join(' ')}</p>
-  </header>
-  <p>${textArea.value}</p>
-  ${imageSrc}
-  `
+function makePost() {
+  makeImage()
+
+  if (textArea.value === '' && imageSrc === '') {
+    return;
+  } else {
+    const newPost = document.createElement('div');
+    postSection.appendChild(newPost);
+    newPost.className = `the-post data`;
+    newPost.innerHTML += `
+    <header class="post-header flex space-between">
+      <div class="flex gap-15">
+        <figure>
+          <img src="./assets/media/profile.jpeg" class="profile">
+        </figure>
+        <h3>Navjot Kaur</h3>
+      </div>
+      <p>${now.toDateString().split(' ').slice(1).join(' ')}</p>
+    </header>
+    <p>${textArea.value}</p>
+    ${imageSrc}
+    `
+  }
 }
 
 listen('click', postButton, () => {
@@ -66,9 +77,11 @@ const subscriber = new Subscriber (
   9787, 'Navjot Kaur', 'nav_chohan0210', 'navchohan@gmail.com', 'Andre Fans', 'Soft Dev', true
 );
 
+let money = '';
+money = subscriber.canMonetize === true ? 'Yes' : 'No';
+
 function displayUserInfo() {
   const userInfo = document.getElementById('modal');
-  const closeBtn = select('.close');
   userInfo.innerHTML = `
     <div class="subscriber-info">
       <img src="./assets/media/profile.jpeg" class="user-image" alt="profile">
@@ -78,7 +91,7 @@ function displayUserInfo() {
         <p><i class="fa-regular fa-envelope"></i>${subscriber.email}</p>
         <p class="pages"><i class="fa-regular fa-file-lines"></i>Pages: ${subscriber.pages}</p>
         <p class="groups"><i class="fa-solid fa-user-group"></i>Groups: ${subscriber.groups}</p>
-        <p><i class="fa-solid fa-sack-dollar"></i>Can Monetize: ${subscriber.canMonetize}</p>
+        <p><i class="fa-solid fa-sack-dollar"></i>Can Monetize: ${money}</p>
       </div>
       <button class="close" id="close" type="button">Close</button>
     <div>
@@ -95,8 +108,22 @@ function displayModal() {
 function closeModal() {
   const modal = document.getElementById('modal');
   modal.style.display = 'none';
+  hideDialog();
+}
+
+function showDialog() {
+  userInfo.classList.add('isvisible');
+  overlay.classList.add('isvisible');
+  isVisible = true;
+}
+
+function hideDialog() {
+  userInfo.classList.remove('isvisible');
+  overlay.classList.remove('isvisible');
+  isVisible = false;
 }
 
 listen('click', profileImg, function() {
   displayModal();
+  showDialog()
 });
